d5786fa834bcb6d2d829bb6b1bcb117f8e591956
whike-chan <10iridescent.symphogear01@gmail.com>
2022-10-13 19:05:30 +0900

    Add scrollTriggerためし

---
+++ scrollTrigger.js
@@ -0,0 +1,54 @@
+// // リストを取得し、指定
+// const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
+// const listEl = document.querySelector('.side-scroll-list');
+
+// // gsap.to()=指定した状態へ変化する
+// gsap.to(listEl, { // アニメーション対象を引数に指定（クラス名を文字列で入れてもok）
+//   x: () => -(listEl.clientWidth - listWrapperEl.clientWidth), // リストの横幅 – リストラッパーの横幅
+//   // ↑関数で指定すると画面のリサイズに対応してくれる
+//   ease: 'none',
+
+//   // scrollTriggerプラグイン
+//   scrollTrigger: {
+//     trigger: '.side-scroll', // .side-scrollが画面内に入ったら実行
+//     start: 'top top', // 要素の上端（top）が、ビューポートの上端（top）にきた時
+//     end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,  // xと同じ
+//     scrub: true, // スクロールに同期する
+//     pin: true, // ピン止めする（スクロールで下に行けない）
+//     anticipatePin: 1, // ガタツキ防止
+//     invalidateOnRefresh: true, // リサイズ設定
+//     markers: true, // デバッグ機能
+//   },
+// });
+
+// unicorn
+const $trigger = document.getElementById('trigger');
+const $wrapper = document.getElementById('wrap');
+const $field = document.getElementById('field');
+
+gsap.to($field, {
+  x: () => -($field.clientWidth - $wrapper.clientWidth),
+  ease: 'none',
+
+  scrollTrigger: {
+    trigger: $trigger,
+    start: 'top top',
+    end: () => `+=${$field.clientWidth - $wrapper.clientWidth}`,
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    invalidateOnRefresh: true,
+    markers: true,
+  }
+})
+
+let anime = gsap.timeline();
+ScrollTrigger.create({
+  animation: anime,
+  trigger: ".side-scroll",
+  start: "top top",
+  end: () => `+=${$field.clientWidth - $wrapper.clientWidth}`,
+  scrub: true,
+})
+
+anime.to('#img', { x: 10, y: -10, duration: 1});
